327616ed46b06119002f55b091bbedf8e1c7f5d4
albanie <albanie@robots.ox.ac.uk>
2018-08-04 14:27:39 +0100

    add automatic fetching

--- FER2013/getFerImdb.m
+++ FER2013/getFerImdb.m
@@ -9,6 +9,10 @@ function imdb = getFerImdb(dataDir)
 
   rawFaceDims = [48 48 1] ;
   dataPath = fullfile(dataDir, 'fer2013.csv') ;
+
+  if ~exist(dataPath, 'file')
+    fetchDataset('fer2013', dataPath) ;
+  end
   csvData = importdata(dataPath) ;
 
   % parse data
@@ -39,3 +43,46 @@ function imdb = getFerImdb(dataDir)
   imdb.meta.sets = {'train', 'val', 'test'} ;
   imdb.meta.classes = {'anger', 'disgust', 'fear', ...
                        'happiness', 'sadness', 'surprise', 'neutral'} ;
+
+% -----------------------------------------
+function fetchDataset(datasetName, dataPath)
+% -----------------------------------------
+
+  waiting = true ;
+  prompt = sprintf(strcat('%s was not found at %s\nWould you like to ', ...
+          ' download it from THE INTERNET (y/n)?\n'), datasetName, dataPath) ;
+
+  while waiting
+    str = input(prompt,'s') ;
+    switch str
+      case 'y'
+        dataDir = fileparts(dataPath) ;
+        if ~exist(dataDir, 'dir'), mkdir(dataDir) ; end
+        fprintf(sprintf('Downloading %s ... \n', datasetName)) ;
+        baseUrl = 'http://www.robots.ox.ac.uk/~albanie/data/mirrors' ;
+        url = sprintf('%s/%s.tar', baseUrl, datasetName) ;
+        % write the tar file to the same directory and unpack
+        archivePath = fullfile(dataDir, sprintf('%s.tar', datasetName)) ;
+        urlwrite(url, archivePath) ;
+        % matlab builtin untar keeps breaking, so use cmd line
+        cmd = sprintf('tar -xvf %s -C %s', archivePath, dataDir) ;
+        status = system(cmd) ;
+        if status > 0
+          error(['automatic extraction failed, it will be necessary to '...
+                                    'set up the dataset manually\n']) ;
+        end
+        % move data up a directory
+        srcPath = fullfile(dataDir, 'fer2013/fer2013.csv') ;
+        destPath = fullfile(dataDir, 'fer2013.csv') ;
+        status = movefile(srcPath, destPath) ;
+        if status ~= 1 % note that MATLAB uses 1 for a success :(
+          error(['directory layout failed, it will be necessary to '...
+                                    'set up the dataset manually\n']) ;
+        end
+        delete(archivePath) ; % clean up
+        fprintf('successfully extracted the dataset!\n') ;
+        return ;
+      case 'n', throw(exception) ;
+      otherwise, fprintf('input %s not recognised, please use `y/n`\n', str) ;
+    end
+  end
